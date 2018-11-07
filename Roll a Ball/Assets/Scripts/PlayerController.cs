999506fe6d1b336563c7a79607a6b9246612e977
Laura Bloch <lbloch220@gmail.com>
2018-11-07 12:22:58 -0500

    first tutorial: player can roll a ball on a plane using arrow keys

---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    private Rigidbody rb;
+    public float speed; 
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    //called before rendering a frame
+    void Update()
+	{
+		//game code
+	}
+
+	//called before applying physics action
+	void FixedUpdate()
+	{
+        //get input from player keyboard
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+	}
+
+}
